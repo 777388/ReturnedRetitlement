3b215df410fcfd8b561ac392c1b4e156c8f79f0c
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-02-17 11:53:25 -0800

    Add files via upload

---
+++ retitlement.py
@@ -0,0 +1,12 @@
+import requests
+
+class retitlement:
+    def __init__(self):
+        ([requests.NullHandler]).extend.__init__(self)
+    def __init_subclass__(self):
+        ([requests.NullHandler]).extend.__init__(self, self)
+class returned(retitlement):
+    def __init__(self):
+        super(returned, self).__init__()
+allot = returned, retitlement
+allot 
